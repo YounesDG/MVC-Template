abf7976215fccbd29640e9a7f167081b2268efbf
YOUNES-RSDT <younes.abderrahmane.dgrsdt@gmail.com>
2021-04-01 09:39:11 +0100

    Administration

--- public/index.php
+++ public/index.php
@@ -7,14 +7,21 @@ define('VIEW_PATH', dirname(__DIR__) . "/views");
 showHideErrors();
 
 
-
 //var_dump(VIEW_PATH);
-$router=new DGRSDT_CANDIDAT\Router(VIEW_PATH);
+$router = new DGRSDT_CANDIDAT\Router(VIEW_PATH);
 $router
-->get('/','/index','Home')
-->get('/notfound','/404','error')
-->get('/candidat/[i:id]','/candidats/index','candidat')
-->run();
+    ->get('/', '/index', 'Home')
+    ->get('/notfound', '/404', 'error')
+    ->get('/candidat/[i:id]', '/candidats/index', 'candidat')
+
+
+    /**Administartion **/
+    ->get('/admin', 'admin/index', 'admin-dashboard')
+    ->get('/admin', 'admin/login', 'admin-login')
+    ->get('/admin', 'admin/profile', 'admin-profile')
+    ->get('/admin', 'admin/register', 'admin-register')
+    ->get('/admin', 'admin/table', 'admin-table')
+    ->run();
 
 
 
--- src/Router.php
+++ src/Router.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DGRSDT_CANDIDAT;
 
 use AltoRouter;
@@ -6,81 +7,88 @@ use DGRSDT\Security\ForbiddenException;
 
 class Router
 {
-    
+
     /**
-    * @var string
-    */
+     * @var string
+     */
     private $viewPath;
-    
+
     /**
-    * @var AltoRouter
-    */
+     * @var AltoRouter
+     */
     private $router;
-    
+
     public function __construct(string $viewPath)
     {
         $this->viewPath = $viewPath;
         $this->router = new \AltoRouter();
     }
-    
+
     public function get(string $url, string $view, ?string $name = null): self
     {
         $this->router->map('GET', $url, $view, $name);
-        
+
         return $this;
     }
-    
+
     public function post(string $url, string $view, ?string $name = null): self
     {
         $this->router->map('POST', $url, $view, $name);
-        
+
         return $this;
     }
-    
+
     public function match(string $url, string $view, ?string $name = null): self
     {
         $this->router->map('POST|GET', $url, $view, $name);
-        
+
         return $this;
     }
-    
+
     public function url(string $name, array $params = [])
     {
         return $this->router->generate($name, $params);
     }
-    
-    
+
+
     public function run(): self
     {
-        
-        $router=$this;
-        $layout =  'layouts/default';
+
+        $router = $this;
+        $layout = 'layouts/default';
         $match = $this->router->match();
 
-        if( is_array($match) ) {
-            
+        if (is_array($match)) {
+
             $view = $match['target'] ?: '404';
             $params = $match['params'];
-             
+
+            //layouts
+            $isAdmin = strpos($view, 'admin/') !== false;
+
+
+
+
+            if ($layout = $isAdmin) {
+                $layout = 'admin/layouts/default';
+            } else {
+                $layout = 'layouts/default';
+            }
+
             try {
                 ob_start();
                 require $this->viewPath . DIRECTORY_SEPARATOR . $view . '.php';
-                $content=ob_get_clean();
-                require $this->viewPath . DIRECTORY_SEPARATOR . $layout . '.php';     
+                $content = ob_get_clean();
+                require $this->viewPath . DIRECTORY_SEPARATOR . $layout . '.php';
             } catch (ForbiddenException $e) {
                 header('Location: ' . $this->url('Home'));
                 exit();
             }
-        }else{
+        } else {
             header('Location: ' . $this->url('Home'));
         }
         return $this;
     }
-    
-    
-    
-    
-    
-    
-    
+
+
 }
